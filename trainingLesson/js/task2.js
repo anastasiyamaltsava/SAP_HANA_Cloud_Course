e70bb697d4ae0c2d94171ee08ff3eec9ae64bf7b
anastasiya.maltsava <31538418+anastasiyamaltsava@users.noreply.github.com>
2019-01-13 15:41:26 +0300

    Update task2.js

--- trainingLesson/js/task2.js
+++ trainingLesson/js/task2.js
@@ -1,16 +1,22 @@
 var container = document.getElementById("container");
 var main = document.getElementById("main");
+var mainBody = document.getElementById("main-body");
+var headImg = document.getElementById("head-img");
+var headName = document.getElementById("head-name");
+
+var names = [ 'Ира', 'Кира', 'Маша', 'Миша', 'Костя', 'Саша', 'Даша', 'Дима', 'Женя', 'Настя' ];
 
 for (let index = 1; index < 11; index++) {
 
     var card = document.createElement("div");
     card.setAttribute("class", "card");
+    card.setAttribute("id", "card");
 
     var img = document.createElement("img");
     img.setAttribute("src", "img/icon" + index + ".jpg");
 
     var text = document.createElement("div");
-    text.textContent = "Name";
+    text.textContent = names[index - 1];
 
     card.appendChild(img);
     card.appendChild(text);
@@ -19,13 +25,13 @@ for (let index = 1; index < 11; index++) {
 
 }
 
-for (let i = 0; i < 2; i++) {
+for (let i = 0; i < 3; i++) {
 
     var messageFrom = document.createElement("div");
     messageFrom.setAttribute("class", "message from");
 
     var img = document.createElement("img");
-    img.setAttribute("src", "img/icon1.jpg");
+    img.setAttribute("class", "imgFrom");
 
     var messageText = document.createElement("div");
     messageText.setAttribute("class", "message-text");
@@ -34,11 +40,11 @@ for (let i = 0; i < 2; i++) {
     messageFrom.appendChild(img);
     messageFrom.appendChild(messageText);
 
-    main.appendChild(messageFrom);
+    mainBody.appendChild(messageFrom);
 
 }
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 4; i++) {
 
     var messageFrom = document.createElement("div");
     messageFrom.setAttribute("class", "message to");
@@ -53,6 +59,24 @@ for (let i = 0; i < 3; i++) {
     messageFrom.appendChild(img);
     messageFrom.appendChild(messageText);
 
-    main.appendChild(messageFrom);
+    mainBody.appendChild(messageFrom);
 
 }
+
+Array.from(document.querySelectorAll('.card')).forEach(function (e, i) {
+    e.onclick = function () {
+        document.getElementById("main-head").style.display = "flex";
+
+        headImg.setAttribute("src", "img/icon" + (i + 1) + ".jpg");
+        headName.textContent = names[i];
+
+        Array.from(document.querySelectorAll('.imgFrom')).forEach(function (el, ind) {
+            el.setAttribute("src", "img/icon" + (i + 1) + ".jpg");
+        });
+
+        mainBody.style.display = "block";
+        main.style.bottom = "0";
+        main.style.top = "0";
+        main.style.position = "relative";
+    }
+});
